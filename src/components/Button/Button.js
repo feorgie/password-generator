f1b6f139d22fe28c282d34f256059865ff5e9788
feorgie <phil.smith.drums@gmail.com>
2024-07-31 12:05:35 +0200

    button svg arrow styled

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./Button.module.css";
+import { Arrow } from "./Arrow";
 
 const Button = ({ onClick }) => {
+  const [hoverActive, setHoverActive] = useState(false);
+  const toggleHover = () => {
+    setHoverActive(!hoverActive);
+  };
   return (
-    <button onClick={onClick} className={css.button}>
+    <button
+      onClick={onClick}
+      onMouseEnter={toggleHover}
+      onMouseLeave={toggleHover}
+      className={css.button}
+    >
       GENERATE
+      <Arrow className={css.arrow} isHoverActive={hoverActive} />
     </button>
   );
 };
