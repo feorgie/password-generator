c499f8650cc16f0050af58af4334402a02cce2e1
feorgie <phil.smith.drums@gmail.com>
2024-07-31 08:53:59 +0200

    adds custom slider

--- src/components/RangeSelect/RangeSelect.js
+++ src/components/RangeSelect/RangeSelect.js
@@ -1,21 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import css from "./RangeSelect.module.css";
+
 const RangeSelect = ({ length, handleSliderChange }) => {
+  const max = 20;
+  const min = 1;
+  const value = 10;
+
+  const [inputValue, setInputValue] = useState(value);
+  const [sliderRange, setSliderRange] = useState(value);
+  const sliderRef = useRef(null);
+
+  const handleSliderInput = () => {
+    const range = max - min;
+    const distance = sliderRef.current.value - min;
+    const percentage = (distance / range) * 100;
+    setSliderRange(percentage);
+    setInputValue(sliderRef.current.value);
+    handleSliderChange(sliderRef.current.value);
+  };
+  useEffect(() => {
+    handleSliderInput();
+  }, [sliderRef]);
+
   return (
-    <div>
+    <>
       <div className={css.textContainer}>
         <p className="fontBody">Character length</p>
         <p className="fontLarge green">{length}</p>
       </div>
-      <input
-        type="range"
-        min={1}
-        max={20}
-        step={1}
-        value={length}
-        onChange={(e) => handleSliderChange(e.currentTarget.value)}
-      />
-    </div>
+      <div className={css.sliderContainer}>
+        <input
+          className={css.slider}
+          type="range"
+          min={1}
+          max={20}
+          step={1}
+          value={inputValue}
+          ref={sliderRef}
+          onInput={handleSliderInput}
+        />
+        <div
+          className={css.sliderThumb}
+          style={{ left: `calc(${sliderRange}%)` }}
+        ></div>
+        <div
+          className={css.progress}
+          style={{ width: `${sliderRange}%` }}
+        ></div>
+      </div>
+    </>
   );
 };
 
